20a0468245cdc52f1c6f71aff1b49009b6538b61
Joni Turunen <joni.turunen@triplan.fi>
2022-09-07 21:14:41 +0300

    task: Tests pass remove obsolete test cases

--- main_test.go
+++ main_test.go
@@ -1,41 +1,10 @@
 package main
 
 import (
-	"fmt"
-	"strings"
 	"testing"
 )
 
-func TestUserInput(t *testing.T) {
-	t.Run("Get user input", func(t *testing.T) {
-		input := fmt.Sprintln(10)
-		rdr := strings.NewReader(input)
-		got := getUserInput(rdr)
-		expected := 10
-
-		if got != expected {
-			t.Errorf("got %d want %d", got, expected)
-		}
-	})
-}
-
 func TestArabicNumeralsToRomanNumerals(t *testing.T) {
-	t.Run("Convert 1 to I", func(t *testing.T) {
-		got := converToRomanNumeral(1)
-		expected := "I"
-
-		if got != expected {
-			t.Errorf("got %q want %q", got, expected)
-		}
-	})
-	t.Run("Convert 5 to V", func(t *testing.T) {
-		got := converToRomanNumeral(5)
-		expected := "V"
-
-		if got != expected {
-			t.Errorf("got %q want %q", got, expected)
-		}
-	})
 	t.Run("Convert 9 to IX", func(t *testing.T) {
 		got := converToRomanNumeral(9)
 		expected := "IX"
